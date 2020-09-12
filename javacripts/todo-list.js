77f0e489d22df32122ddc1f204e18c30ff059aa6
Nerbyk <danil.maximov2000@gmail.com>
2020-09-12 12:26:46 -0400

    moduled TodoStrage class again..

--- javacripts/todo-list.js
+++ javacripts/todo-list.js
@@ -1,39 +1,7 @@
 const createForm = document.createElement('FORM')
 const todosElement = document.getElementById("todos")
 
-class TodoStorage {
-    constructor() {
-        this.todos = []
-        this.id = 0
-    }
-    newTodo(value, status) {
-        this.id++;
-        let todoObj = { id: this.id, value: value, status: status };
-        this.todos.push(todoObj);
-    }
-
-    changeStatusOf(id) {
-        let elementIndex = this.todos.findIndex(element => element.id == id)
-        let cloneTodos = [...this.todos]
-        cloneTodos[elementIndex] = {...cloneTodos[elementIndex],
-            status: !cloneTodos[elementIndex].status
-        }
-        this.todos = cloneTodos
-    }
-
-    toStorage() {
-        localStorage.setItem('todos', JSON.stringify(this.todos))
-    }
-
-    fromStorage() {
-        let storedData = localStorage.getItem('todos')
-        return JSON.parse(storedData)
-    }
-
-    empty() {
-        localStorage.clear();
-    }
-}
+import TodoStorage from './todoStorage.js';
 const storage = new TodoStorage
 
 function generateItems(itemText, isChecked) {
